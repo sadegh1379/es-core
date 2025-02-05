089e4e1c2d232a11db0b1c9e77fa553a7edeff67
Sadegh Akbari <akbarisadegh382@gmail.com>
2025-02-05 14:49:56 +0330

    fix: next build action

--- scripts/build-helper.js
+++ scripts/build-helper.js
@@ -17,7 +17,7 @@ async function createPackageFile() {
         peerDependencies: {
             react: '^18.2.0',
             'react-dom': '^18.2.0',
-            'next': '^14.2.3'
+            'next': '^14.2.3',
         },
     };
 
--- scripts/clean-package.sh
+++ scripts/clean-package.sh
@@ -6,4 +6,4 @@ sed -i '/"react-dom": "\^18\.2\.0",/d' ./package.json
 sed -i '/"next": "\^14\.2\.3",/d' ./package.json
 
 # Step 2: Add lines before JSON closing tag
-sed -i '/^\}$/i \  ,"peerDependencies": {\n    "react": "\^18\.2\.0",\n    "react-dom": "\^18\.2\.0",\n  "next": "\^14\.2\.3",\n  }' ./package.json
+sed -i '/^\}$/i \  ,"peerDependencies": {\n    "react": "\^18\.2\.0",\n    "react-dom": "\^18\.2\.0",\n  "next": "\^14\.2\.3"\n  }' ./package.json
